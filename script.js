3115d0807eabb5825418b6140ddb1f45f421dea0
Federico Trotta <ftrotta18@gmail.com>
2023-09-11 14:04:35 -0300

    Refactorizacion del codigo, parte comentarios en archivo separado

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const spinner = document.getElementById("spinner");
-let comentarios = [];
-const comentarBTN = document.getElementById("comentarBTN");
+
 const searchButton = document.getElementById("searchButton");
 
 document.addEventListener("DOMContentLoaded", async () => {
-  comentarBTN.addEventListener("click", handleCommentBTN);
-
   searchButton.addEventListener("click", async () => {
     spinner.classList.remove("hideSpinner");
     const pokemonName = document
@@ -16,17 +13,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.getElementById("pokemonInfo").innerHTML = result;
     spinner.classList.add("hideSpinner");
   });
-
-  //agarro mi archivo local
-  const commentsDeArchivo = await fetch("./comments.json");
-  const commentsDeArchivoData = await commentsDeArchivo.json();
-
-  // si no existe local storage = crear local storage
-  comentarios =
-    JSON.parse(localStorage.getItem("comments")) || commentsDeArchivoData;
-
-  //mostrar los comentarios del local
-  showAllComments(comentarios);
 
   const url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
   const response = await fetch(url);
@@ -219,59 +205,4 @@ const handlePagination = async (pagina) => {
     result += await fetchPokemones(pokemon.url);
   }
   document.getElementById("pokeArray").innerHTML = result;
-};
-
-const handleAddLike = (id) => {
-  let singleComment = comentarios.find((comment) => comment.id == id);
-  singleComment.isLiked = !singleComment.isLiked;
-
-  //remuevo el objeto viejo y guardo el nuevo en el local storage
-  refreshStorage();
-
-  //volver a  mostrar en la lista de comentarios el local storage
-  showAllComments(comentarios);
-};
-
-const showAllComments = (array_comments) => {
-  let result = "";
-  for (const comment of array_comments) {
-    result += ` 
-    <div>
-      <h4>${comment.usuario}</h4>
-      <p>${comment.comentario}</p>
-      ${
-        comment.isLiked
-          ? ` <i onclick="handleAddLike(${comment.id})" class="fa-solid fa-heart"></i>`
-          : ` <i onclick="handleAddLike(${comment.id})" class="fa-regular fa-heart"></i>`
-      }
-    </div>`;
-  }
-  document.getElementById("showAllComments").innerHTML = result;
-};
-
-const handleCommentBTN = () => {
-  //agarrar los inputs
-  const userInput = document.getElementById("userInput").value;
-  const userComment = document.getElementById("userComment").value;
-
-  const commentarioAlLocal = {
-    id: comentarios.length + 1,
-    usuario: userInput,
-    comentario: userComment,
-    isLiked: false,
-  };
-
-  //agregar al local storage
-  comentarios.push(commentarioAlLocal);
-
-  //remuevo el objeto viejo y guardo el nuevo en el local storage
-  refreshStorage();
-
-  //volver a  mostrar en la lista de comentarios el local storage
-  showAllComments(comentarios);
-};
-
-const refreshStorage = () => {
-  localStorage.removeItem("comments");
-  localStorage.setItem("comments", JSON.stringify(comentarios));
 };
